1deb59c3867af2679be9d87d129b9fa910e28208
Saurabh Kumar <thes.kumar@gmail.com>
2014-01-12 21:41:05 +0550

    Site Updated

--- js/plugins/jquery.trottle.debounce.js
+++ js/plugins/jquery.trottle.debounce.js
@@ -6,5 +6,4 @@
  * Dual licensed under the MIT and GPL licenses.
  * http://benalman.com/about/license/
  */
-
-(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+!function(t,n){var u,e=t.jQuery||t.Cowboy||(t.Cowboy={});e.throttle=u=function(t,u,o,i){function r(){function e(){c=+new Date,o.apply(f,g)}function r(){a=n}var f=this,d=+new Date-c,g=arguments;i&&!a&&e(),a&&clearTimeout(a),i===n&&d>t?e():u!==!0&&(a=setTimeout(i?r:e,i===n?t-d:t))}var a,c=0;return"boolean"!=typeof u&&(i=o,o=u,u=n),e.guid&&(r.guid=o.guid=o.guid||e.guid++),r},e.debounce=function(t,e,o){return o===n?u(t,e,!1):u(t,o,e!==!1)}}(this);
