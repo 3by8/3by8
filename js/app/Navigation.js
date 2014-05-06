2c0105400d9e33ab72ca837b0a17b88bec8714a3
Saurabh Kumar <me@saurabh-kumar.com>
2014-05-07 02:51:12 +0550

    b5234e1 replace isotope by monasary

--- js/app/Navigation.js
+++ js/app/Navigation.js
@@ -1,1 +1,1 @@
-!function(){var t,n;n="undefined"!=typeof exports&&null!==exports?exports:this,t=function(){function t(){}return t.prototype.log=function(t){return alert("Nav Says"+t)},t}(),n.Navigation||(n.Navigation=t)}.call(this);
+(function(){var t,n;n="undefined"!=typeof exports&&null!==exports?exports:this,t=function(){function t(){}return t.prototype.log=function(t){return alert("Nav Says"+t)},t}(),n.Navigation||(n.Navigation=t)}).call(this);
